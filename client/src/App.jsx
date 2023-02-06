6f4d387fa377d805190d6105049f89fc0edeefa5
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-06 19:36:44 +0100

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,15 @@ import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  return;
-  <BrowserRouter>
-    <header claseName="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]"></header>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <header claseName="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+      </header>
+    </BrowserRouter>
+  );
 };
 
 export default App;
