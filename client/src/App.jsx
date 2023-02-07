d3f0caab94f203e9f974ecf03fbfcffe3fd7db56
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-07 20:53:36 +0100

    <main className="sm:p-8 px-4 py-8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
           Create
         </Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]"></main>
     </BrowserRouter>
   );
 };
