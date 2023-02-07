28cf175ce0bf0f8eee9ec4ac78ba0a03d98fdd7e
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-07 21:44:18 +0100

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Loader, Card, FromField } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  const [allPost, setAllPost] = useState([null]);
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+        <p></p>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
