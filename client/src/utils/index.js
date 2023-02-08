629a3d279d49893d7f2ab0c91735d510b9238b48
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-08 22:07:06 +0100

    export function getRandomPrompt(prompt)

---
+++ client/src/utils/index.js
@@ -0,0 +1,7 @@
+import { supriseMePrompts } from "../constants";
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * supriseMePrompts.length);
+  const randomPrompt = supriseMePrompts[randomIndex];
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+  return randomPrompt;
+}
