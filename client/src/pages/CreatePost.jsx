bc659a2b915c4e5e7abbc113bf84a64f22ed4a15
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-08 22:00:18 +0100

    create Post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPromt } from "..utils";
+import { FormField, Loader } from "componenets";
 
 const CreatePost = () => {
   return <h1>create post</h1>;
