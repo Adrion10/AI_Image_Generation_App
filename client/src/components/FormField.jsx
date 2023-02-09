7d819faf0fffb4cdce19815a7c4dde34e7df0c63
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-09 23:37:14 +0100

    FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -7,40 +7,35 @@ const FormField = ({
   placeholder,
   value,
   handleChange,
-  isSupriseMe,
-  handleSupriseMe,
-}) => {
-  return (
-    <div>
-      <div className="flex item-center gap-2 mb-2">
-        <label
-          htmlFor={name}
-          className="block text-sm font-medium text-gray-900"
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => (
+  <div>
+    <div className="flex items-center gap-2 mb-2">
+      <label htmlFor={name} className="block text-sm font-medium text-gray-900">
+        {labelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
         >
-          {labelName}
-        </label>
-        {isSupriseMe && (
-          <button
-            type="button"
-            onClick={handleSupriseMe}
-            className="font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px]text-black"
-          >
-            Suprise Me
-          </button>
-        )}
-        <input
-          type={type}
-          id={name}
-          name={name}
-          placeholder={placeholder}
-          value={value}
-          onChange={handleChange}
-          required
-          className="bg-gray-50 border border-gray-300 text gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[4649ff] outline-none block w-full p-3 "
-        />
-      </div>
+          Surprise me
+        </button>
+      )}
     </div>
-  );
-};
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+    />
+  </div>
+);
 
 export default FormField;
