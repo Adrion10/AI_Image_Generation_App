dfa8c6c28154d27f3f132fb3d622138a8488e01c
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-09 22:45:15 +0100

    fixed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { preview } from "../assets";
-import { getRandomPromt } from "../utils";
+import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
