a695e54aa99c106aea444526accef4debfc1fa16
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-09 23:14:46 +0100

    const FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  labelNeme,
+  type,
+  name,
+  palceholder,
+  value,
+  handleChange,
+  isSupriseMe,
+  handleSupriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex item-center gap-2 mb-2"></div>
+    </div>
+  );
 };
 
 export default FormField;
