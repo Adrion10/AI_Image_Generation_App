e87064da25d667099c1b83c2a25c2d5b28c50b93
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-11 23:03:42 +0100

    fixed

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ dotenv.config();
 const app = express();
 app - use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.get("/", asymc (req, res) => {res.send('Hello from DALL-E')
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E");
 });
-
