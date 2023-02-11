6ccd986c17bb220f2f6aea0d0d6836c814d380ad
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-11 23:07:05 +0100

    const startServer

--- server/index.js
+++ server/index.js
@@ -10,3 +10,8 @@ app.use(express.json({ limit: "50mb" }));
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E");
 });
+const startServer = async () => {
+  app.listen(8080, () =>
+    console.log("SServer has started on port://localhost:8080")
+  );
+};
