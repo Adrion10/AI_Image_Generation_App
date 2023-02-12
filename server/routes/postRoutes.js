7c0bedfb79ec9afe34569926c7090696612849f2
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-12 21:55:48 +0100

    postRoutes

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudianry } from "cloudinary";
+import Post from "../mongoDB/models/post.js";
+
+dotenv.config();
+const router = express.Router();
