9d9529fd62171e08980819e7d0458d5225380992
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-12 22:13:08 +0100

    dalleRoutes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -4,3 +4,13 @@ import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from Dall-E");
+});
+export default router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,3 +5,5 @@ import Post from "../mongoDB/models/post.js";
 
 dotenv.config();
 const router = express.Router();
+
+export default router;
