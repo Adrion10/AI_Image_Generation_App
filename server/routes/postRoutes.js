c77d7716c0e3aa9314032b2555ad2d2a23871dbc
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-13 19:41:45 +0100

    postRouter

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -11,5 +11,17 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+// GET ALL POSTS
+router.route("/").get(async (req, res) => {});
+//Cretae a Post
+router.route("/").post(async (req, res) => {
+  const { name, prompt, photo } = req.body;
+  const photoUrl = await cloudinary.uploader.uploadr(photo);
+  const newPost = await Post.create({
+    name,
+    propmt,
+    photo: photoUrl.url,
+  });
+});
 
 export default router;
