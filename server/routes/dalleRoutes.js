f07632507ca14fa815f9f59bba230e841ea1675d
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-13 15:17:54 +0100

    router.route("/").post(async

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,4 +13,16 @@ const openai = new OpenAIApi(configuration);
 router.route("/").get((req, res) => {
   res.send("Hello from Dall-E");
 });
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = aiResponse.data.data[0].b64_json;
+  } catch (error) {}
+});
 export default router;
