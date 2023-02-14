2f91398b07f96c92bf92ee913cd7012a10ab9fe8
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-14 23:47:22 +0100

    postRouter

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
+
 import Post from "../mongoDB/models/post.js";
 
 dotenv.config();
+
 const router = express.Router();
 
 cloudinary.config({
@@ -11,25 +13,37 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-// GET ALL POSTS
+
 router.route("/").get(async (req, res) => {
   try {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
   }
 });
-//Cretae a Post
+
 router.route("/").post(async (req, res) => {
-  const { name, prompt, photo } = req.body;
-  const photoUrl = await cloudinary.uploader.uploadr(photo);
-  const newPost = await Post.create({
-    name,
-    prompt,
-    photo: photoUrl.url,
-  });
-  res.status(201).json({ success: true, data: newPost });
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
 });
 
 export default router;
