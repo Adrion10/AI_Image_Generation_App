f953ff5fc436fdba5b7ea5329f5e00d234feaea7
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-14 22:33:28 +0100

    headers:{'Content-Type':'application/json'

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -53,6 +53,22 @@ try {
     if(form.prompt && form.photo){
       setLoading(true)
 
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method:"POST",
+          headers:{'Content-Type':'application/json',},
+          body:JSON.stringify(form)
+        })
+        await response.json()
+        navigate('/')
+        
+      } catch (err) {
+        alert(err)
+        
+      }finally{
+        setLoading(false)
+      }
+
     }
   };
 
