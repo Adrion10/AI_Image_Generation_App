d4f7a3eb84f7faba17b774ccb2a2400c5c108ba0
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-14 23:10:02 +0100

    method:'GET',

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,10 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post')
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method:'GET',
+          headers: {'Content-Type': 'application/json'}
+        })
       } catch (error) {
 
       }finally {
