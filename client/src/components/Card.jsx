4b3cd32e19520897a52c4e03ad418ec9f99fdea7
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-15 20:34:44 +0100

    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,7 +3,15 @@ import { download } from "../assets";
 import { downloadImage } from "../utils";
 
 const Card = (_id, name, prompt, photo) => {
-  return <div>Card</div>;
+  return (
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+      <img
+        className="w-full h-auto object-cover rounded-xl"
+        src={photo}
+        alt={prompt}
+      />
+    </div>
+  );
 };
 
 export default Card;
