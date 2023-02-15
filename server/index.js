34de3c83fa51488040ce77d7c3d6223e02f621d7
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-15 21:56:54 +0100

    mongoDb

--- server/index.js
+++ server/index.js
@@ -12,21 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use("/app/v1/post", postRoutes);
-app.use("/app/v1/dalle", dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-  res.send("Hello from Lace");
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
 });
+
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-
-    app.listen(8080, () =>
-      console.log("Server has started on port http://localhost:8080")
-    );
+    app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
-    console.log("error");
+    console.log(error);
   }
 };
+
 startServer();
