c35cd574df08b00a10a5895397893b282043e9a0
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-15 20:26:09 +0100

    npm i file-saver

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
-import FileSaver from "file.sarver";
+import FileSaver from "file-saver";
 import { surpriseMePrompts } from "../constants";
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
