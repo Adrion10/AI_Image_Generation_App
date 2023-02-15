579dc16479abf23fb642cbd2c9af566bc9a26cf8
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-15 20:41:39 +0100

    <p classNmae="text-white text-sm

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = (_id, name, prompt, photo) => {
         alt={prompt}
       />
       <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f]m-2 p-4 rounded-md ">
-        <p></p>
+        <p classNmae="text-white text-sm overflow-y-auto prompt"></p>
       </div>
     </div>
   );
