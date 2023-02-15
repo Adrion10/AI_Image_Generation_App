6a7d99850337cb8a5fb90fd1bc88bf4a99f22dee
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-15 21:01:12 +0100

    <button             type="button"             onClick={() => downloadImage

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = (_id, name, prompt, photo) => {
         alt={prompt}
       />
       <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f]m-2 p-4 rounded-md ">
-        <p classNmae="text-white text-sm overflow-y-auto prompt">{prompt} </p>
+        <p classNmae="text-white text-md overflow-y-auto prompt">{prompt} </p>
         <div classNmae="mt-5 flex justify-beteween items-center gap-2">
           <div className="flex items-center gap-2">
             <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">
@@ -19,6 +19,17 @@ const Card = (_id, name, prompt, photo) => {
             </div>
             <p className="text-white text-sm">{name} </p>
           </div>
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, photo)}
+            className="outline-none bg-transparent border-none"
+          >
+            <img
+              src={download}
+              alt="downlad"
+              className="w-6 h-6 object-contain-invert"
+            />
+          </button>
         </div>
       </div>
     </div>
